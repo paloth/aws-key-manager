cdf847df9a2c9ecf5a6bed17547777d708c99310
Paul LOUIS THERESE <paul.louis-therese@d2si.io>
2020-05-25 17:14:05 +0200

    refactor: Restart project

--- cmd/generate.go
+++ cmd/generate.go
@@ -1,36 +1,47 @@
 /*
 Copyright © 2020 Paloth
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-    http://www.apache.org/licenses/LICENSE-2.0
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
 */
 package cmd
 
 import (
 	"fmt"
-	"os"
 
+	"github.com/paloth/aws-key-manager/internal/profile"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 var userName string
-var token string
-var profile string
+var userToken string
+var userProfile string
 
 // generateCmd represents the generate command
 var generateCmd = &cobra.Command{
 	Use:   "generate",
-	Short: "Generate a temporary acces key with mfa",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		run()
 	},
@@ -38,48 +49,17 @@ var generateCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(generateCmd)
+
 	generateCmd.Flags().StringVarP(&userName, "username", "u", "", "The AWS user name")
-	generateCmd.Flags().StringVarP(&profile, "profile", "p", "", "The AWS user profile")
-	generateCmd.Flags().StringVarP(&token, "token", "t", "", "The user token")
+	generateCmd.Flags().StringVarP(&userProfile, "profile", "p", "", "The AWS user profile")
+	generateCmd.Flags().StringVarP(&userToken, "token", "t", "", "The user token")
 
 	generateCmd.MarkFlagRequired("username")
 	generateCmd.MarkFlagRequired("token")
 	generateCmd.MarkFlagRequired("profile")
 }
 
-const (
-	credentialFile string = "/.aws/credentials"
-)
-
-var (
-	home        string
-	profileList []string
-)
-
 func run() {
-	profile.getConfig()
-	getConfig()
-}
-
-func getHomeValue(h *string) {
-	var err error
-	*h, err = os.UserHomeDir()
-	if err != nil {
-		fmt.Println("Error: ", err)
-		os.Exit(2)
-	}
-}
-
-func getConfig() {
-	getHomeValue(&home)
-	viper.SetConfigFile("awsProfile")
-	viper.SetConfigType("toml")
-	viper.AddConfigPath(home + credentialFile)
-
-	err := viper.ReadInConfig()
-	if err != nil {
-		panic(fmt.Errorf("%s", err))
-	}
-
-	viper.Get("")
+	fmt.Println("yo")
+	profile.GetConfig()
 }
