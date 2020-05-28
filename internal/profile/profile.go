08762d2c365d5abbaaf82afa08e515bbf58321c9
Paul LOUIS THERESE <paul.louis-therese@d2si.io>
2020-05-28 23:16:52 +0200

    feat: Check user profile

--- internal/profile/profile.go
+++ internal/profile/profile.go
@@ -9,7 +9,7 @@ import (
 )
 
 const (
-	credentialFile string = "/.aws/"
+	credentialFile string = "/.aws/credentials"
 )
 
 var (
@@ -18,26 +18,31 @@ var (
 )
 
 func getHomeValue(h *string) {
+
 	var err error
 	*h, err = os.UserHomeDir()
 	if err != nil {
 		fmt.Println("Error: ", err)
 		os.Exit(2)
 	}
+
 }
 
-func GetConfig() configparser.ConfigParser {
+func getConfig() configparser.ConfigParser {
+
 	getHomeValue(&home)
 
 	config, err := configparser.NewConfigParserFromFile(home + credentialFile)
 	if err != nil {
-		panic(fmt.Errorf("Error: %s", err))
+		fmt.Printf("Error: %s", err)
 		os.Exit(2)
 	}
 	return *config
+
 }
 
 func removeBadProfile(list *configparser.ConfigParser) []string {
+
 	var listProfile []string
 
 	for i := 0; i < len(list.Sections()); i++ {
@@ -51,16 +56,21 @@ func removeBadProfile(list *configparser.ConfigParser) []string {
 
 	}
 	return listProfile
+
 }
 
-func CheckProfile(profile string, config *configparser.ConfigParser) error {
+func CheckProfile(profile string) error {
+
+	config := getConfig()
 	if exists := config.HasSection(profile); exists {
-		return nil
+		flag, err := config.HasOption(profile, "aws_access_key_id")
+		if err != nil {
+			fmt.Println(err)
+		}
+		if flag == true && !strings.HasSuffix(profile, "-tmp") {
+			return nil
+		}
+		return fmt.Errorf("You have entered an invalid profile")
 	}
 	return fmt.Errorf("The profile is not present in your configuration file")
-}
-
-// Main function
-func Main() {
-	println("Profile")
 }
