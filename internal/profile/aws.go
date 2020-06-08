5f1f735c9456bd7bb7696abf68b81b45b9e27974
Paul LOUIS THERESE <paul.louis-therese@d2si.io>
2020-06-08 19:20:02 +0200

    feat: check user entries and call aws api

---
+++ internal/profile/aws.go
@@ -0,0 +1,41 @@
+package profile
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sts"
+)
+
+func GetAwsSession(profile string, user string, token string) sts.GetSessionTokenOutput {
+	awsSession, err := session.NewSessionWithOptions(session.Options{
+		Profile: profile,
+	})
+	if err != nil {
+		if awsErr, ok := err.(awserr.Error); ok {
+			fmt.Println(awsErr)
+		}
+	}
+	svcSts := sts.New(awsSession)
+
+	identity, err := svcSts.GetCallerIdentity(&sts.GetCallerIdentityInput{})
+	if err != nil {
+		if awsErr, ok := err.(awserr.Error); ok {
+			fmt.Println(awsErr)
+		}
+	}
+
+	session, err := svcSts.GetSessionToken(&sts.GetSessionTokenInput{
+		DurationSeconds: aws.Int64(43200),
+		SerialNumber:    aws.String("arn:aws:iam::" + *identity.Account + ":mfa/" + user),
+		TokenCode:       aws.String(token),
+	})
+	if err != nil {
+		if awsErr, ok := err.(awserr.Error); ok {
+			fmt.Println(awsErr)
+		}
+	}
+	return *session
+}
--- internal/profile/profile.go
+++ internal/profile/profile.go
@@ -3,8 +3,10 @@ package profile
 import (
 	"fmt"
 	"os"
+	"regexp"
 	"strings"
 
+	"github.com/aws/aws-sdk-go/service/sts"
 	"github.com/bigkevmcd/go-configparser"
 )
 
@@ -14,36 +16,41 @@ const (
 
 var (
 	home        string
+	config      *configparser.ConfigParser
 	profileList []string
+	err         error
+	reToken     = regexp.MustCompile(`[0-9]{6}`)
 )
 
-func getHomeValue(h *string) {
+func CheckToken(token string) error {
+	if reToken.MatchString(token) {
+		return nil
+	}
+	return fmt.Errorf("The token %s must be composed by six digits", token)
+}
 
-	var err error
+func getHomeValue(h *string) {
 	*h, err = os.UserHomeDir()
 	if err != nil {
 		fmt.Println("Error: ", err)
 		os.Exit(2)
 	}
-
 }
 
-func getConfig() configparser.ConfigParser {
-
+func getConfig() *configparser.ConfigParser {
 	getHomeValue(&home)
 
-	config, err := configparser.NewConfigParserFromFile(home + credentialFile)
+	cfg, err := configparser.NewConfigParserFromFile(home + credentialFile)
 	if err != nil {
 		fmt.Printf("Error: %s", err)
 		os.Exit(2)
 	}
-	return *config
-
+	return cfg
 }
 
 func CheckProfile(profile string) error {
+	config = getConfig()
 
-	config := getConfig()
 	if exists := config.HasSection(profile); exists {
 		flag, err := config.HasOption(profile, "aws_access_key_id")
 		if err != nil {
@@ -54,5 +61,29 @@ func CheckProfile(profile string) error {
 		}
 		return fmt.Errorf("You have entered an invalid profile")
 	}
-	return fmt.Errorf("The profile is not present in your configuration file")
+	return fmt.Errorf("The profile %s is not present in your configuration file", profile)
+}
+
+func WriteConfigFile(profile string, session *sts.GetSessionTokenOutput) error {
+	if exists := config.HasSection(profile + "-tmp"); exists {
+		config.Set(profile+"-tmp", "aws_access_key_id", *session.Credentials.AccessKeyId)
+		config.Set(profile+"-tmp", "aws_secret_access_key", *session.Credentials.SecretAccessKey)
+		config.Set(profile+"-tmp", "aws_session_token", *session.Credentials.SessionToken)
+		config.Set(profile+"-tmp", "aws_default_region", "eu-west-1")
+	} else {
+		fmt.Println("Profile " + profile + "-tmp does not exists.")
+		config.AddSection(profile + "-tmp")
+		fmt.Println("Profile " + profile + "-tmp has been created.")
+		config.Set(profile+"-tmp", "aws_access_key_id", *session.Credentials.AccessKeyId)
+		config.Set(profile+"-tmp", "aws_secret_access_key", *session.Credentials.SecretAccessKey)
+		config.Set(profile+"-tmp", "aws_session_token", *session.Credentials.SessionToken)
+		config.Set(profile+"-tmp", "aws_default_region", "eu-west-1")
+	}
+
+	err := config.SaveWithDelimiter(home+credentialFile, "=")
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
