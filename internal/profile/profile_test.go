4699fc38e8d142c72d4538d29ec613dbbae26889
Paul LOUIS THERESE <paul.louis-therese@d2si.io>
2020-06-09 11:47:12 +0200

    test: add basic test

--- internal/profile/profile_test.go
+++ internal/profile/profile_test.go
@@ -1,7 +1,19 @@
 package profile
 
-import "testing"
+import (
+	"fmt"
+	"testing"
 
-func TestCheckToken (t *testing.T) {
-	
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCheckToken(t *testing.T) {
+	result := CheckToken("123456")
+	assert.Nil(t, result)
+
+	result = CheckToken("1234")
+	assert.NotNil(t, result)
+
+	result = CheckToken("1234")
+	assert.Equal(t, result, fmt.Errorf("The token 1234 must be composed by six digits"), "error")
 }
