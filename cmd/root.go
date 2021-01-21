c364c103ad126f12782bed85491ad8dde8e664e4
paloth <lth.paul@hotmail.fr>
2021-01-21 15:41:09 +0100

    docs: add program description

--- cmd/root.go
+++ cmd/root.go
@@ -33,10 +33,7 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "aws-key-manager",
-	Short: "A brief description of your application",
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	Short: "Simply manage your AWS personnal access key",
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
