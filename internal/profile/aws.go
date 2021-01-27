8d936c0a9ce6be6e1076e55fb59102eee770c152
paloth <lth.paul@hotmail.fr>
2021-01-27 16:17:41 +0100

    feat: check existing ak before delete

--- internal/profile/aws.go
+++ internal/profile/aws.go
@@ -54,3 +54,53 @@ func CreateNewAccess(session *iam.IAM, profile string, user *string) (iam.Create
 
 	return *newAccessKey, nil
 }
+
+func DeleteOldAccessKey(session *iam.IAM, oldAk *string, user *string) error {
+	_, err := session.DeleteAccessKey(&iam.DeleteAccessKeyInput{
+		AccessKeyId: oldAk,
+		UserName:    user,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func CheckAkNumber(session *iam.IAM, user *string) (bool, error) {
+	accessKeys, err := session.ListAccessKeys(&iam.ListAccessKeysInput{
+		UserName: user,
+	})
+	if err != nil {
+		return false, err
+	}
+	if len(accessKeys.AccessKeyMetadata) >= 2 {
+		fmt.Println("Too much accessKeys for the user %s", user)
+		selectKeyToDelete(&accessKeys.AccessKeyMetadata)
+	}
+	return true, nil
+}
+
+func selectKeyToDelete(list []*iam.AccessKeyMetadata) (bool, error){
+	switch runtime.GOOS {
+	case "windows":
+		for index, value := range list {
+			fmt.Println("%d - %s", index, value)
+		}
+
+	default:
+		prompt := promptui.Select{
+			Label: "Select AccessKey to delete",
+			Items: list,
+		}
+	
+		_, result, err := prompt.Run()
+	
+		if err != nil {
+			fmt.Printf("Prompt failed %v\n", err)
+			return
+		}
+	
+		fmt.Printf("You choose %q\n", result)
+	return true, nil
+}
